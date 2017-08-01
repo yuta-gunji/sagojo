a9083f818ab017010a746fc1e188c55cf3bf4bbb
Yuta Gunji <yuta.g05@gmail.com>
2017-08-01 15:00:06 +0900

    define routing for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+  }
   root   'works#index'
+  resources :users, only: [:edit, :update]
   resources :works, only: :index
 end
