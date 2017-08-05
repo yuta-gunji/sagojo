47deb831fb64c733650ba7d7aee132890b81186a
Yuta Gunji <yuta.g05@gmail.com>
2017-08-05 15:53:18 +0900

    Create user_category model & user_categories table

---
+++ db/migrate/20170805023123_create_user_categories.rb
@@ -0,0 +1,9 @@
+class CreateUserCategories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :user_categories do |t|
+      t.references  :user, null: false, foreign_key: true
+      t.references  :category, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
