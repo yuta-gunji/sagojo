a52c18b00d3e0c8b37c3ec139a2fbe0ec0be4bcc
Yuta Gunji <yuta.g05@gmail.com>
2017-08-14 18:25:14 +0900

    Change kind of storage

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
 
   process convert: 'jpg'
 
