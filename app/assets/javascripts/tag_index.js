eb38ca938d855f54f43d58c4d839ed4f0ca43dc1
Yuta Gunji <yuta.g05@gmail.com>
2017-08-14 15:42:10 +0900

    Modified for incremental search & adding input of tag

--- app/assets/javascripts/tag_index.js
+++ app/assets/javascripts/tag_index.js
@@ -1,28 +1,50 @@
 $(function() {
 
+  var tag_num = $('.tag-select__lists__list').length;
+
   function buildTagList(tag) {
     var html = `<li class="tag-search-result__list">
                   <a class="tag-search-result__list--add", data-tag-id="${ tag.id }" data-tag-name="${ tag.name }">
-                   ${ tag.name }
+                    ${ tag.name }
                   </a>
-               </li>`
-    return html;
+                </li>`
+    $("#tag-search-result").append(html);
   }
 
-  function addTag(tag) {
+  function selectTag(tag) {
     var tag_id = tag.attr('data-tag-id');
     var tag_name = tag.attr('data-tag-name');
-    var html = `<li class="tag-select-lists__list">
-                  <input name='work[tag_ids][]' type='hidden' value="${ tag_id }" >
-                  <span class='tag-select-lists__list--name'>${ tag_name }</span>
-                  <a class='tag-select-lists__list--remove'>×</a>
-                </li>`
-
-    $('.tag-select-lists').prepend(html);
+    var html =
+        `<li class="tag-select__lists__list" id="add_tag_${tag_num}">
+           <input name='work[tag_ids][]' type='hidden' value="${ tag_id }">
+           <span class='tag-select__lists__list--name'>${ tag_name }</span>
+           <span class="tag-select__lists__list--delete" data-id="${ tag_num }", data-default="default">
+             ×
+           </span>
+         </div>`
+    $('.tag-select__lists').prepend(html);
   }
 
-  $(".tag-select-lists__new--input").on("keyup", function() {
-    var input = $(".tag-select-lists__new--input").val();
+  function addTag(input) {
+    var html =
+        `<li class="tag-select__lists__list" id="add_tag_${tag_num}">
+           <input class="tag-select__lists__list--added" type="hidden" name="work[tags_attributes][${ tag_num }][name]" id="tags_attributes_${tag_num}_name" value="${ input }">
+           <span class='tag-select__lists__list--name'>${ input }</span>
+           <a class="tag-select__lists__list--delete" data-id="${ tag_num }", data-default="default">
+             ×
+           </a>
+         </div>`
+      $('.tag-select__lists').prepend(html);
+  }
+
+  $(".tag-select__lists__input--field").on("keyup", function() {
+    var input = $('.tag-select__lists__input--field').val();
+    if(input !== "") {
+      $('.tag-select__lists__input--add').css('display', 'inline-block');
+    }
+    else {
+      $('.tag-select__lists__input--add').css('display', 'none')
+    }
     $.ajax({
       type: 'GET',
       url: '/tags',
@@ -34,8 +56,7 @@ $(function() {
       $("#tag-search-result").empty();
       if (input.length !== 0 && tags.length !== 0) {
         tags.forEach(function(tag) {
-          var html = buildTagList(tag);
-          $("#tag-search-result").append(html);
+          buildTagList(tag);
         });
       }
     })
@@ -43,20 +64,37 @@ $(function() {
       alert('検索に失敗しました');
     })
   });
-  $(".tag-select-lists").on("click", function() {
-    $(".tag-select-lists__new--input").focus();
+
+  $(".tag-select__lists").on("click", function() {
+    $(".tag-select__lists__input--field").focus();
   });
-  $(".tag-select-lists__new--input").on("focus", function() {
-    $(".tag-select-lists").css('border-color', '#ffcb00');
-  });
+
   $(document).on("click", ".tag-search-result__list--add", function() {
     $(this).parent().remove();
-    addTag($(this));
-    $('.tag-select-lists__new--input').val('');
+    selectTag($(this));
+    $('.tag-select__lists__input--field').val('');
+    $('.tag-select__lists__input--add').css('display', 'none');
+    tag_num ++;
   });
 
-  $(document).on("click", ".tag-select-lists__list--remove", function() {
+  $(document).on("click", ".tag-select__lists__list--remove", function() {
     $(this).parent().remove();
-
   });
+
+  $('.tag-select__lists__input--add').on('click', function() {
+    var input = $('.tag-select__lists__input--field').val();
+    if(input !== "") {
+      addTag(input);
+      $('.tag-select__lists__input--field').val('');
+      $('.tag-select__lists__input--add').css('display', 'none');
+      tag_num ++;
+    }
+  });
+
+  $(document).on('click', '.tag-select__lists__list--delete', function() {
+    var inputId = $(this).data('id');
+    var defaultData = $(this).data('default');
+    $('#add_tag_' + inputId).remove();
+  });
+
 });
