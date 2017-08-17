34556df683758a5ee094ab8034827888ec8c9853
Yuta Gunji <yuta.g05@gmail.com>
2017-08-17 20:33:39 +0900

    Add description for nested_attributes & image_uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   has_many :user_tags
   has_many :categories, through: :user_categories
   has_many :tags, through: :user_tags
+  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :tags
+  mount_uploader :avatar, ImageUploader
 end
