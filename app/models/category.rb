516a17e1ab6c8a27ae0513eac42b20e213adcf52
Yuta Gunji <yuta.g05@gmail.com>
2017-08-19 17:51:02 +0900

    Add association with intermediate table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  has_many :user_categories
+  has_many :work_categories
   has_many :users, through: :user_categories
   has_many :works, through: :work_categories
   validates :name, null: false
