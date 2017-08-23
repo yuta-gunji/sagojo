7977adee37fd25c6c0c0d39542f62f7fb050fa12
Yuta Gunji <yuta.g05@gmail.com>
2017-08-23 12:49:35 +0900

    Edited to add tag_count_number

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,14 @@
 class TagsController < ApplicationController
 
   def index
-    @tags = Tag.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @tags = []
+    tags = Tag.where('name LIKE(?)', "%#{params[:keyword]}%")
+    tags.each do |tag|
+      t_hash = {}
+      t_count = UserTag.where(tag_id: tag.id).count
+      t_hash = {tag: tag, count: t_count}
+      @tags << t_hash
+    end
     respond_to do |format|
       format.json
     end
