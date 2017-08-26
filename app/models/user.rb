910b305557d6331bb41b65b75923b53094953191
Yuta Gunji <yuta.g05@gmail.com>
2017-08-26 20:26:55 +0900

    Edited for 'tagit'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true, uniqueness: true
-  has_many :user_categories
-  has_many :user_tags
-  has_many :categories, through: :user_categories
-  has_many :tags, through: :user_tags
   has_many :candidates
   has_many :works, through: :candidates
-  accepts_nested_attributes_for :categories
-  accepts_nested_attributes_for :tags
   mount_uploader :avatar, ImageUploader
+  acts_as_ordered_taggable_on :categories, :skills
 end
