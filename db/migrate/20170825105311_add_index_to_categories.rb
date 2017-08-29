cf7c36a96c4195e9eddfa8d2f873a5bb35cb8415
Yuta Gunji <yuta.g05@gmail.com>
2017-08-29 16:49:50 +0900

    migration 1file sakujo

--- db/migrate/20170825105311_add_index_to_categories.rb
+++
@@ -1,8 +0,0 @@
-class AddIndexToCategories < ActiveRecord::Migration[5.0]
-  def change
-    remove_index :categories, :name
-    remove_index :tags, :name
-    add_index :categories, :name, unique: true
-    add_index :tags, :name, unique: true
-  end
-end
